fcee15462a43705f1b5813134155f30aed0f1ed5
andresjz94 <luis.juarez@contrastsecurity.com>
2021-01-28 10:20:42 -0600

    BLANK-7
    Entities

---
+++ blank/src/main/java/mx/dev/blank/entity/Cashier.java
@@ -0,0 +1,43 @@
+package mx.dev.blank.entity;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "cashier")
+@EqualsAndHashCode(of = {"id"})
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Cashier implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(name = "id", nullable = false)
+    private String id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "location", nullable = false)
+    private String location;
+
+    @Column(name = "amount", nullable = false)
+    private BigDecimal amount;
+
+}

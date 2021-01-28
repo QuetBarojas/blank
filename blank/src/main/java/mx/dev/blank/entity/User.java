5e06686f52713f773803d1d078d9a2ac0afeecec
Jesus Cotlame <jesuscotlame@gmail.com>
2021-01-28 14:17:37 -0600

    changes by ide

--- blank/src/main/java/mx/dev/blank/entity/User.java
+++ blank/src/main/java/mx/dev/blank/entity/User.java
@@ -1,19 +1,18 @@
 package mx.dev.blank.entity;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.hibernate.annotations.GenericGenerator;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import java.io.Serializable;
-import java.math.BigDecimal;
 
 @Entity
 @Table(name = "user")
@@ -22,26 +21,26 @@ import java.math.BigDecimal;
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User implements Serializable {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(generator = "uuid")
-    @GenericGenerator(name = "uuid", strategy = "uuid2")
-    @Column(name = "id", nullable = false)
-    private String id;
+  @Id
+  @GeneratedValue(generator = "uuid")
+  @GenericGenerator(name = "uuid", strategy = "uuid2")
+  @Column(name = "id", nullable = false)
+  private String id;
 
-    @Column(name = "name", nullable = false)
-    private String name;
+  @Column(name = "name", nullable = false)
+  private String name;
 
-    @Column(name = "surname", nullable = false)
-    private String surname;
+  @Column(name = "surname", nullable = false)
+  private String surname;
 
-    @Column(name = "token", nullable = false)
-    private String token;
+  @Column(name = "token", nullable = false)
+  private String token;
 
-    @Column(name = "cardNumber", nullable = false)
-    private String cardNumber;
+  @Column(name = "cardNumber", nullable = false)
+  private String cardNumber;
 
-    @Column(name = "balance", nullable = false)
-    private BigDecimal balance;
+  @Column(name = "balance", nullable = false)
+  private BigDecimal balance;
 }
